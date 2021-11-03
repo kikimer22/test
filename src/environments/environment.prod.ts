eda1947f20fe6da6762ad9c7ca799529ce266e0d
ruslansalnikov <ruslan.salnikov@cd.p2h.com>
2021-11-03 23:13:27 +0200

    init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { Environment } from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCSFTgEryAQZejDrC928UlLTKkTGJeGKvA',
+  fbDbUrl: 'https://test031121-94e42-default-rtdb.europe-west1.firebasedatabase.app'
+};
